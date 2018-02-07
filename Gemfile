376595a54d516ecec935f984f4e50ebe37397a6f
abhishekkanojia <abhishek.kanojia@vinsol.com>
2018-02-07 18:32:43 +0530

    Initialized

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+
+gem 'rails', '~> 5.1.4'
+
+gem 'sqlite3'
+
+gem 'puma', '~> 3.7'
+
+gem 'sass-rails', '~> 5.0'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.2'
+
+gem 'turbolinks', '~> 5'
+
+gem 'bcrypt', '~> 3.1.7'
+
+# Use Capistrano for deployment
+# gem 'capistrano-rails', group: :development
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+end
---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users, path_names: { new: "user_new", edit: "user_edit" }
+
+  # resources :users, path: "user" # /user/new, /user
+
+  # resources :users, only: [:new, :index]
+
+  # resources :users, except: [:new, :index]
+
+  # resources :users, shallow: true do
+  #   resources :comments
+  # end
+
+  # resources :users, shallow: true
+
+  # scope shallow_path: "sekret" do
+  #   resources :users, shallow: true
+  # end
+
+  # scope shallow_path: "sekret" do
+  #   resources :users, shallow: true do
+  #     resources :comments
+  #   end
+  # end
+
+  # resources :users, format: false
+end
