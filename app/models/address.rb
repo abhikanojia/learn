5f4d02173af8695e1bbbf999006da6e3a684517c
Abhishek kanojia <abhishekka3193@gmail.com>
2018-02-11 20:42:54 +0530

    Sti

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 class Address < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
 
   # validates_presence_of :city, :street, :postal_code
+  # validates_length_of :city, minimum: 2
+
+  # Address.first => first entry
+  # BillingAddress.first => first entry of billing address using In('BillingAddress')
 end
--- app/models/billing_address.rb
+++ app/models/billing_address.rb
@@ -1,3 +1,4 @@
 class BillingAddress < Address
   validates_presence_of :country
+  validates_length_of :city, minimum: 4
 end
