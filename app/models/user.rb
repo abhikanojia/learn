7533001be83a33277d4237f73d3c85672ab85785
abhishekkanojia <abhishek.kanojia@vinsol.com>
2018-02-14 17:16:11 +0530

    started callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :addresses
+
+  before_validation :ensure_user_has_name
+  after_validation :after_validation_callback
+
+  private
+
+    def ensure_user_has_name
+      puts "checking ensure_user_has_name.."
+    end
+
+    def after_validation_callback
+      puts "after_validation_callback"
+    end
 end
